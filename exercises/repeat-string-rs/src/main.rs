2447adce2d20c33a3c0a76862559a4224b4dbf57
alejandro Lopez <alejandrolpz0619@gmail.com>
2022-01-27 17:26:34 -0400

    🚀Changes: ✔ exercises/repeat-string-rs/src/main.rs ✔ exercises/repeat-string-rs/Cargo.toml ✔ exercises/repeat-string-rs/Cargo.lock ✔ exercises/repeat-string-ts/main.ts

---
+++ exercises/repeat-string-rs/src/main.rs
@@ -0,0 +1,6 @@
+fn repeat_str(src: &str, count: usize) -> String {
+    src.repeat(count)
+  }
+fn main() {
+    repeat_str("Hello", 5);
+}
---
+++ exercises/repeat-string-ts/main.ts
@@ -0,0 +1,5 @@
+function repeatString(src: String, count: number): String {
+  return src.repeat(count);
+}
+
+console.log(repeatString("Ale", 5))
